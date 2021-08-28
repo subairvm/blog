a564fb936522a0d744f782af97540bd31f1b83ee
subairvm <subairvm69@gmail.com>
2021-08-28 21:54:40 +0530

    blog

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import { Route, Switch } from 'react-router-dom';
+import Contact from './Contact';
+import Home from './Home';
+import Post from './Post';
+import About from './About';
+import Navbar from './Layout/Navbar';
+import './index.css'
 
-function App() {
-  return (
-    <h1>HOME</h1>
-    </div>
-  );
-}
 
-export default App;
+const App = () => {
+  return(
+    <>
+    <Navbar/>
+    <Switch>
+      <Route exact path ="/" component={Home}/>
+      <Route path ="/about" component={About}/>
+      <Route path ="/contact" component={Contact}/>
+      <Route path ="/post" component={Post}/>
+    </Switch>
+  
+  </>
+   )
+  }
+  export default App;
